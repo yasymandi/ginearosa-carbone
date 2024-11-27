dd238418aa604e89448094d740e0e6bbbf48d069
Yasy Mandi <yasymandi@gmail.com>
2024-11-27 13:06:02 -0800

    Taking out little bubble quick access things

--- src/Bio/bio.tsx
+++ src/Bio/bio.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './bio.css';
 import author_pic from './Ginearosa-author-pic.jpg';
 import { bioText } from './bio-text';
-import podcast_img from './podcast_img.png'
 
 const Bio: React.FC = () => {
   
@@ -15,31 +14,6 @@ const Bio: React.FC = () => {
               {bioText}
             </div>
               <img src={author_pic} className="author-pic" />
-          </div>
-          <div className='quick-access'>
-            <a className='personal'
-              href="https://www.amazon.com/Cut-Me-Open-Make-Whole/dp/1946874876" target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://m.media-amazon.com/images/I/71rFtvzJ1wL._SL1500_.jpg"
-                className="bubble-pic"
-                alt="Book Cover"
-              />
-              <span className="tooltip">book</span>
-            </a>  
-            <a
-              className='personal'
-              href="https://podcasts.apple.com/us/podcast/cutting-open-ginearosa-carbone-and-not-leaving-any-wholes/id680438389?i=1000615657219"
-              target="_blank"
-              rel="noopener noreferrer"
-              
-            >
-              <img
-                src={podcast_img}
-                className="bubble-pic"
-                alt="Podcast Pic"
-              />
-              <span className="tooltip">podcast feature</span>
-              </a>        
           </div>
         </div>
     </div>
